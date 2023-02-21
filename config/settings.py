2b60a2b0151b79da622e93abbee04e1cccbcf3e4
tchappui <tchappui@gmail.com>
2023-02-21 09:45:11 +0100

    Added media folder

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ WEBPACK_LOADER = {
 }
 
 MEDIA_URL = "media/"
-STATIC_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
