c7a98ba6bbe901463c8547f62d1c3100585f04e7
tchappui <tchappui@gmail.com>
2023-03-27 17:24:08 +0200

    Updated static files

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,8 @@
 import pathlib
-import sys
 
 import environ
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
-sys.path.insert(0, BASE_DIR / "wepynaire")
 
 env = environ.Env()
 env.read_env(BASE_DIR / ".env")
